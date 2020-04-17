b9941ddab1b593982e10cb8a8c4293e630b43735
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-04-17 12:05:10 -0400

    Challenge Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MagicBall());
+
+class MagicBall extends StatefulWidget{
+  @override
+  MagicBallState createState() => MagicBallState();
+
+}
+
+class MagicBallState extends State<MagicBall>{
+
+  int ballNumber = 1;
+
+  generateRandomNumber()
+  {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Magic Ball',
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          centerTitle: true,
+          title: Text(
+              'Ask me anything',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: Center(
+          child: FlatButton(
+            child: Image.asset('images/ball$ballNumber.png'),
+            onPressed: () {
+              generateRandomNumber();
+            },
+          ),
+        ),
       ),
     );
+  }
+}
